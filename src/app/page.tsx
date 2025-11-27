2414603179739b84c1c59ac9826301357b1b614c
Matheus7p <matheus.costa.7p@gmail.com>
2025-11-27 12:24:34 -0300

    feat: page to inform that the application is under construction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import { ReactNode } from "react";
 
-export default function Home (): ReactNode {
+export default async function Home (): Promise<ReactNode> {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   return (
-    <>
-      <h1 className="text-purple-900 text-4xl font-bold">Duo Forge</h1>
-    </>
+    <main className="flex items-center justify-center min-h-screen px-4">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4 text-white-900">
+          Duo Forge
+        </h1>
+        <h3 className="text-4xl font-bold mb-4 text-white-900">
+          Under Construction
+        </h3>
+
+        <p className="text-lg text-white-700 max-w-md mx-auto">
+          This application is currently under development.  
+          We are working hard to bring you something amazing.  
+          Please check back soon!
+        </p>
+
+        <p className="mt-6 text-sm text-white-500">
+          Thank you for your patience.
+        </p>
+      </div>
+    </main>
   );
 }
--- test/app/page.test.tsx
+++ test/app/page.test.tsx
@@ -3,8 +3,11 @@ import { render, screen } from "@testing-library/react";
 import Home from "@/app/page";
 
 describe("Page", () => {
-  it("should display the text \"Duo Forge\"", () => {
-    render(<Home />);
+  it("should display the text \"Duo Forge\"", async () => {
+    // Act
+    render( await Home());
+
+    // Assert
     expect(screen.getByText("Duo Forge")).toBeInTheDocument();
   });
 });
